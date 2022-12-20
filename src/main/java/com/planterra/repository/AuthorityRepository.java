fa8b23f94e388923d6867800bc8ec5e681ba60d0
Andra Vasilache <vasilacheandragabriela@gmail.com>
2022-12-20 23:33:33 +0200

    init

---
+++ src/main/java/com/planterra/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.planterra.repository;
+
+import com.planterra.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
